44e49c779662588bb8d805e9c6c0609120fcddee
Hanzhi Teng <tenghanzhi@outlook.com>
2024-09-29 03:34:33 -0700

    add auto-save feature

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { debounce } from "../utils/debounce";
 import mockUserInfo from "../data/mockUserInfo";
 import mockLocationInfo from "../data/mockLocationInfo";
 import "./UserProfile.css";
 
 const LOCATION_API_KEY = "";
 const PROFILE_API_TOKEN = "";
+
 const DEFAULT_PROFILE = {
   name: "",
   age: "",
@@ -19,46 +21,102 @@ const DEFAULT_PROFILE = {
   avatarUri: "",
 };
 
+const SAVE_STATUE = {
+  SAVED: "saved",
+  NOT_SAVED: "not-saved",
+};
+
+const MESSAGES_MATRIX = {
+  CONFIRM_FETCH_LOCATION:
+    "Fetch Location will overwrite existing location information, are you sure to process?",
+  CONFIRM_FETCH_USER_INFO:
+    "Fetch User Info will overwrite existing user information, are you sure to process?",
+  CONFIRM_DELETE_SAVED_PROFILE:
+    "Confirm to delete saved profile and clean the fields?",
+};
+
 const UserProfile = () => {
   const [profile, setProfile] = useState(DEFAULT_PROFILE);
+  const [autoSave, setAutoSave] = useState(true);
+  const [saveStatus, setSaveStatus] = useState(SAVE_STATUE.SAVED);
 
   useEffect(() => {
     handleCheckLocalStorage();
   }, []);
 
+  useEffect(() => {
+    if (autoSave) {
+      debouncedSave();
+    }
+  }, [profile]);
+
   // Fetch user location from API
   const fetchLocation = async () => {
-    if (LOCATION_API_KEY) {
-      try {
-        const response = await axios.get(
-          "https://ipgeolocation.abstractapi.com/v1/",
-          {
-            params: { api_key: LOCATION_API_KEY },
-          }
-        );
-        setProfile((prev) => ({ ...prev, location: response.data.city }));
-      } catch (error) {
-        console.error("Error fetching location:", error);
+    if (
+      (profile.location &&
+        window.confirm(MESSAGES_MATRIX.CONFIRM_FETCH_LOCATION)) ||
+      !profile.location
+    ) {
+      if (LOCATION_API_KEY) {
+        try {
+          const response = await axios.get(
+            "https://ipgeolocation.abstractapi.com/v1/",
+            {
+              params: { api_key: LOCATION_API_KEY },
+            }
+          );
+          setProfile((prev) => ({
+            ...prev,
+            location: response.data.city,
+          }));
+        } catch (error) {
+          console.error("Error fetching location:", error);
+        }
+      } else {
+        setProfile((prev) => ({
+          ...prev,
+          location: mockLocationInfo.city,
+        }));
       }
-    } else {
-      setProfile((prev) => ({ ...prev, location: mockLocationInfo.city }));
     }
   };
 
   // Fetch user information from API
   const fetchUserInfo = async () => {
-    if (PROFILE_API_TOKEN) {
-      try {
-        const response = await axios.get(
-          "https://api-staging-0.gotartifact.com/v2/users/me",
-          {
-            headers: {
-              Authorization: PROFILE_API_TOKEN,
-            },
-          }
-        );
+    const { email, username, displayName, avatarUri } = profile;
+
+    const fieldFilled = email || username || displayName || avatarUri;
+
+    if (
+      (fieldFilled &&
+        window.confirm(MESSAGES_MATRIX.CONFIRM_FETCH_USER_INFO)) ||
+      !fieldFilled
+    ) {
+      if (PROFILE_API_TOKEN) {
+        try {
+          const response = await axios.get(
+            "https://api-staging-0.gotartifact.com/v2/users/me",
+            {
+              headers: {
+                Authorization: PROFILE_API_TOKEN,
+              },
+            }
+          );
+          const { email, username, display_name, avatar_uri } =
+            response.data.profile;
+          setProfile((prev) => ({
+            ...prev,
+            email,
+            username,
+            displayName: display_name,
+            avatarUri: avatar_uri,
+          }));
+        } catch (error) {
+          console.error("Error fetching user info:", error);
+        }
+      } else {
         const { email, username, display_name, avatar_uri } =
-          response.data.profile;
+          mockUserInfo.profile;
         setProfile((prev) => ({
           ...prev,
           email,
@@ -66,19 +124,7 @@ const UserProfile = () => {
           displayName: display_name,
           avatarUri: avatar_uri,
         }));
-      } catch (error) {
-        console.error("Error fetching user info:", error);
       }
-    } else {
-      const { email, username, display_name, avatar_uri } =
-        mockUserInfo.profile;
-      setProfile((prev) => ({
-        ...prev,
-        email,
-        username,
-        displayName: display_name,
-        avatarUri: avatar_uri,
-      }));
     }
   };
 
@@ -98,22 +144,38 @@ const UserProfile = () => {
     }
   };
 
-  // Handle form input change
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfile((prev) => ({ ...prev, [name]: value }));
-  };
-
   // Save profile data to local storage
   const handleSave = () => {
     localStorage.setItem("userProfile", JSON.stringify(profile));
-    alert("Profile saved successfully!");
+    setSaveStatus(SAVE_STATUE.SAVED);
   };
+
+  const debouncedSave = debounce(() => {
+    localStorage.setItem("userProfile", JSON.stringify(profile));
+    setSaveStatus(SAVE_STATUE.SAVED);
+  }, 1000);
 
   // Delete local storage saved profile
   const handleDelete = () => {
-    localStorage.removeItem("userProfile");
-    setProfile(DEFAULT_PROFILE);
+    if (window.confirm(MESSAGES_MATRIX.CONFIRM_DELETE_SAVED_PROFILE)) {
+      localStorage.removeItem("userProfile");
+      setProfile(DEFAULT_PROFILE);
+    }
+  };
+
+  // Handle auto save checkbox change
+  const handleAutoSaveChange = () => {
+    if (!autoSave) {
+      handleSave();
+    }
+    setAutoSave((prev) => !prev);
+  };
+
+  // Handle form input change
+  const handleChange = (e) => {
+    setSaveStatus("not-saved");
+    const { name, value } = e.target;
+    setProfile((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
@@ -148,15 +210,13 @@ const UserProfile = () => {
           onChange={handleChange}
           placeholder="Location"
         />
-        <input
-          type="text"
+        <textarea
           name="interest1"
           value={profile.interest1}
           onChange={handleChange}
           placeholder="Interest 1"
         />
-        <input
-          type="text"
+        <textarea
           name="interest2"
           value={profile.interest2}
           onChange={handleChange}
@@ -190,6 +250,19 @@ const UserProfile = () => {
           onChange={handleChange}
           placeholder="Avatar URI"
         />
+        <div className="auto-save-container">
+          <input
+            type="checkbox"
+            name={"autoSave"}
+            value={autoSave}
+            checked={autoSave}
+            onChange={handleAutoSaveChange}
+          />
+          <span className="auto-save-title">AutoSave</span>
+          <span className={`save-status-${saveStatus}`}>
+            {saveStatus === "not-saved" ? "Not Saved" : "Saved"}
+          </span>
+        </div>
         <button
           className="fetch-location"
           type="button"
@@ -204,7 +277,12 @@ const UserProfile = () => {
         >
           Fetch User Info
         </button>
-        <button className="save-profile" type="button" onClick={handleSave}>
+        <button
+          className="save-profile"
+          type="button"
+          onClick={handleSave}
+          style={{ visibility: autoSave ? "hidden" : "visible" }}
+        >
           Save Profile
         </button>
         <button
